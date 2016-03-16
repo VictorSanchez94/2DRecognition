6328ae1a5ce4254ad9c02b580be47602e275bf45
VictorSanchez94 <vs_zgz94@hotmail.com>
2016-03-16 13:30:20 +0100

    Final version

--- src/Recognition.cpp
+++ src/Recognition.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 using namespace cv;
 
+int TIPOS_FIGURAS = 5;
+string tipos[] = {"circulo", "rectangulo", "rueda", "triangulo", "vagon"};
+double CHI_2_TEST = 9.49;	//0.0.5 error		//13.28;
 
 void otsuMethod (string path) {
 	Mat dstOtsu, drawing, img, img2;
@@ -15,6 +18,7 @@ void otsuMethod (string path) {
 	string path2;
 	path2 = "images/" + path;
 	img = imread(path2, CV_LOAD_IMAGE_GRAYSCALE);
+	imshow("Imagen Original", img);
 
 	//Otsu's thresholding
 	threshold( img, dstOtsu, 0, 255, THRESH_OTSU+THRESH_BINARY_INV );
@@ -22,16 +26,13 @@ void otsuMethod (string path) {
 	findContours(dstOtsu, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0));
 	drawing = Mat(dstOtsu.size(), CV_8UC3);
 	for( int i = 0; i< contours.size(); i++ ){
-		if ( cv::contourArea(contours[i], false) > 40 && contourArea(contours[i], false) < drawing.rows*drawing.cols){
+		if ( cv::contourArea(contours[i], false) > 60 && contourArea(contours[i], false) < drawing.rows*drawing.cols){
 			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 			drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-			for(int col=0; col<drawing.cols; col++){
-				for(int row=0; row<drawing.rows; row++){
-					if (pointPolygonTest(contours[i],Point(col,row),false) == 1){
-						floodFill(drawing, Point(col,row), color, (cv::Rect*)0, cv::Scalar(), 200);
-					}
-				}
-			}
+			vector<Point> tmp = contours.at(i);
+			const Point* elementPoints[1] = {&tmp[0]};
+			int numberOfPoints = (int)tmp.size();
+			fillPoly(drawing, elementPoints, &numberOfPoints, 1, color, 8);
 			contours2.push_back(contours[i]); //Se añade a contours2. Aqui solo se almacenan los blobs válidos por tamaño
 		}
 	}
@@ -50,51 +51,82 @@ void otsuMethod (string path) {
 	vector<Moments> mu(contours.size() );
 	vector<double> ourDescriptors;
 	double hu[7];
+	Mat contourRecognition = Mat(dstOtsu.size(), CV_8UC3);
 	for( int i = 0; i < contours2.size(); i++ ){
-		cout << "Contorno " << i << ":\n";
 		mu[i] = moments( contours2[i], false );
 		HuMoments(mu[i], hu);
-		cout << "\tArea: " << mu[i].m00 <<
-				"\n\tPerimetro: " << arcLength(contours2[i], true) <<
-				"\n\tMomento Inv. 1: " << hu[0] <<
-				"\n\tMomento Inv. 2: " << hu[1] <<
-				"\n\tMomento Inv. 3: " << hu[2] << "\n";
 		ourDescriptors.push_back(mu[i].m00);
 		ourDescriptors.push_back(arcLength(contours2[i], true));
-		ourDescriptors.push_back(hu[0]);
+		//ourDescriptors.push_back(hu[0]);
 		ourDescriptors.push_back(hu[1]);
 		ourDescriptors.push_back(hu[2]);
 	}
 
 	//Se calcula la distancia de Mahalanobis
 	FileStorage fs2("training.yml", FileStorage::READ);
-	vector<double> muMeans(5);
-	vector<double> muVariances(5);
+	vector<double> muMeans(4);
+	vector<double> muVariances(4);
 
-	fs2["ruedaMedia"] >> muMeans;
-	fs2["ruedaVarianza"] >> muVariances;
-	fs2.release();
+	for (int x=0; x<contours2.size(); x++){
+		bool moreThanOne = false;
+		double bestMahalanobis = HUGE_VAL;
+		string bestObjectCandidate = "";
+		drawContours( contourRecognition, contours2, x, Scalar(0,255,0), 1, 8, hierarchy, 0, Point() );
+		vector<Point> tmp = contours2.at(x);
+		const Point* elementPoints[1] = {&tmp[0]};
+		int numberOfPoints = (int)tmp.size();
+		fillPoly(drawing, elementPoints, &numberOfPoints, 1, Scalar(0,255,0), 8);
+		cout << "\nContorno " << x+1 << ":" << endl;
+		int maxDescriptor = x*4;
+		for (int j=0; j<TIPOS_FIGURAS; j++){
 
-	double mahalanobis = 0;
-	for( int i = 0; i < ourDescriptors.size(); i++ ){
-		cout << ourDescriptors[i] << " " << muMeans[i] << " " << muVariances[i] << "\n";
-		mahalanobis += pow(ourDescriptors[i] - muMeans[i],2)/pow(muVariances[i],2);
+			stringstream ssMedia, ssVarianza;
+			ssMedia << tipos[j] << "Media";
+			ssVarianza << tipos[j] << "Varianza";
+
+			fs2[ssMedia.str()] >> muMeans;
+			fs2[ssVarianza.str()] >> muVariances;
+
+			double mahalanobis = 0;
+
+			//Suma de las distancias de Mahalanobis entre cada descriptor con el de train
+			for( int i = maxDescriptor; i < maxDescriptor+4; i++ ){
+				mahalanobis += pow(ourDescriptors[i] - muMeans[i%4],2)/muVariances[i%4];
+			}
+			if(mahalanobis < bestMahalanobis){
+				if(mahalanobis <= CHI_2_TEST && bestMahalanobis <= CHI_2_TEST){
+					moreThanOne = true;
+				}
+				bestMahalanobis = mahalanobis;
+				bestObjectCandidate = tipos[j];
+			}
+			cout << "Distancia de Mahalanobis con " << tipos[j] << ": " << mahalanobis << endl;
+
+		}
+		//Posibles casos de distincion
+		if(bestMahalanobis <= CHI_2_TEST && !moreThanOne){
+			cout << "== El contorno " << x+1 << " es un " << bestObjectCandidate << ". ==" << endl;
+		}else if(bestMahalanobis <= CHI_2_TEST && moreThanOne){
+			cout << "== El contorno " << x+1 << " se parece a más de un objeto conocido. ==" << endl;
+		}else{
+			cout << "== El contorno " << x+1 << " es un objeto desconocido. ==" << endl;
+		}
+		imshow("Contorno a reconocer", contourRecognition);
+		waitKey(0);
 	}
-	cout << "Distancia de Mahalanobis: " << mahalanobis;
+	fs2.release();
 }
 
 
 int main (int argc, const char* argv[]){
-	for(int i=0; i<argc;i++){
-		cout << argv[i] << endl;
-	}
 	if(strcmp(argv[1], "train") == 0){
-		remove("training.yml");
+		cout << "Entrenando..." << endl;
 		train("images/circulo", 5, "circulo", "write");
 		train("images/rectangulo", 5, "rectangulo", "append");
 		train("images/triangulo", 5, "triangulo", "append");
 		train("images/vagon", 5, "vagon", "append");
 		train("images/rueda", 5, "rueda", "append");
+		cout << "Fin del entrenamiento. Se ha generado el fichero 'training.yml'." << endl;
 	}else if(strcmp(argv[1], "recognition") == 0){
 		otsuMethod(argv[2]);
 	}
--- src/Train.cpp
+++ src/Train.cpp
@@ -4,13 +4,14 @@ using namespace std;
 using namespace cv;
 
 void train(string path, int numObjects, string objectType, char* mode) {
+	//Inicializacion de variables
 	Mat dstOtsu, drawing, img, img2;
 	vector<vector<Point> > contours;
 	vector<Vec4i> hierarchy;
 	RNG rng(12345);
 	double min, max;
-	vector<double> muMeans(5);
-	vector<double> muVariances(5);
+	vector<double> muMeans(4);
+	vector<double> muVariances(4);
 	double hu[7];
 
 	for (int i=1; i<=numObjects; i++){
@@ -27,14 +28,13 @@ void train(string path, int numObjects, string objectType, char* mode) {
 		vector<Moments> mu(contours.size() );
 
 		for( int i = 0; i < contours.size(); i++ ){
-			//cout << "Contorno " << i << ":\n";
 			mu[i] = moments( contours[i], false );
 			HuMoments(mu[i], hu);
 			muMeans[0] += mu[i].m00;
 			muMeans[1] += arcLength(contours[i], true);
-			muMeans[2] += hu[0];
-			muMeans[3] += hu[1];
-			muMeans[4] += hu[2];
+			//muMeans[2] += hu[0];
+			muMeans[2] += hu[1];
+			muMeans[3] += hu[2];
 		}
 	}
 	//Se calcula la media
@@ -42,8 +42,7 @@ void train(string path, int numObjects, string objectType, char* mode) {
 		muMeans[i] = muMeans[i]/numObjects;
 	}
 
-
-	//Se calcula la varianza
+	vector<double> muVariances1(5);
 	for (int i=1; i<=numObjects; i++){
 		stringstream ss;
 		ss << path << i << ".pgm";
@@ -57,23 +56,37 @@ void train(string path, int numObjects, string objectType, char* mode) {
 		//Se calculan los momentos de la imagen
 		vector<Moments> mu(contours.size() );
 		for( int i = 0; i < contours.size(); i++ ){
-			//cout << "Contorno " << i << ":\n";
-			mu[i] = moments( contours[i], false );
-			HuMoments(mu[i], hu);
-			muVariances[0] += pow(mu[i].m00 - muMeans[0],2);
-			muVariances[1] += pow(arcLength(contours[i], true) - muMeans[1],2);
-			muVariances[2] += pow(hu[0] - muMeans[2],2);
-			muVariances[3] += pow(hu[1] - muMeans[3],2);
-			muVariances[4] += pow(hu[2] - muMeans[4],2);
+			if ( cv::contourArea(contours[i], false) > 60 && contourArea(contours[i], false) < drawing.rows*drawing.cols){
+				mu[i] = moments( contours[i], false );
+				HuMoments(mu[i], hu);
+				muVariances[0] += pow(mu[i].m00 - muMeans[0],2);
+				muVariances[1] += pow(arcLength(contours[i], true) - muMeans[1],2);
+				//muVariances[2] += pow(hu[0] - muMeans[2],2);
+				muVariances[2] += pow(hu[1] - muMeans[2],2);
+				muVariances[3] += pow(hu[2] - muMeans[3],2);
+			}
 		}
+		/*if(i==1){ //Se almacenan aparte los descriptores de la primera imagen para regularizar
+			muVariances1[0] = muVariances[0];
+			muVariances1[1] = muVariances[1];
+			muVariances1[2] = muVariances[2];
+			muVariances1[3] = muVariances[3];
+			muVariances1[4] = muVariances[4];
+			muVariances[0] = 0;
+			muVariances[1] = 0;
+			muVariances[2] = 0;
+			muVariances[3] = 0;
+			muVariances[4] = 0;
+		}*/
 	}
 
-	//Se calcula la varianza
+	//Se calcula la varianza regularizada
 	for (int i=0; i<muMeans.size(); i++){
-			muVariances[i] = muVariances[i]/numObjects;
+		//muVariances[i] = muVariances[i]/(numObjects-1);
+		muVariances[i] = pow(0.01*muVariances[i],2)/numObjects + muVariances[i]*(numObjects-1)/numObjects;
 	}
 
-	//TODO: Muy cutre esto
+	//Guardar entrenamiento en fichero
 	if(strcmp(mode, "write") == 0){
 		FileStorage fs("training.yml", FileStorage::WRITE);
 		stringstream media;
